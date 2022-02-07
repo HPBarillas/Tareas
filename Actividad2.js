32e08c887505eb66e9b54e3d30b2fa386ac76070
Hector Pineda <hp.barillas@gmail.com>
2022-02-06 23:34:03 -0600

    Agregando la Actividad2 al repositorio

---
+++ Actividad2.js
@@ -0,0 +1,31 @@
+var mes = 10;
+
+switch(mes){
+  case 1:
+  case 2:
+    console.log("Es Invierno");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Es Primavera");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Es Verano");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("Es Otoño");
+    break;
+  case 12:
+    console.log("Es Invierno");
+    break;
+  default:
+    console.log("Favor de escribir un numero de mes valido");
+}
+
+console.log("Nombre: Hector Eduardo Pineda Barillas");
+console.log("Carnet: 22000174");
